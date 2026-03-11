3d6cc12e5d5212cee090a3db768d5464d8e6fab0
Chetan7528 <kaklotarchetan7528@gmail.com>
2026-03-11 10:23:55 +0530

    all changes done

---
+++ android/app/src/main/java/com/smartwatchios/fitcloud/FitCloudProPackage.kt
@@ -0,0 +1,16 @@
+package com.smartwatchios.fitcloud
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class FitCloudProPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(FitCloudProModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
